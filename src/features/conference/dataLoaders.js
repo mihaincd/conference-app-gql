bdc2114de50a8a76348d35f56b7846cb3acc89ed
mihaincd <danut49@gmail.com>
2021-09-30 13:05:22 +0300

    small changes

--- src/features/conference/dataLoaders.js
+++ src/features/conference/dataLoaders.js
@@ -2,8 +2,6 @@ const DataLoader = require('dataloader')
 
 const getConferencesLoaders = dbInstance => {
   return {
-    
-    
     locationById: new DataLoader(ids =>
       dbInstance
         .select('Id', 'Name', 'Address', 'Latitude', 'Longitude', 'CityId', 'CountyId', 'CountryId')
@@ -11,16 +9,14 @@ const getConferencesLoaders = dbInstance => {
         .whereIn('Id', ids)
         .then(rows => ids.map(id => rows.find(x => x.id === id)))
     ),
-    speakersByConferenceId: new DataLoader(ids => 
+    speakersByConferenceId: new DataLoader(ids =>
       dbInstance
         .select('s.Id', 's.Name', 's.Nationality', 's.Rating', 'c.ConferenceId', 'c.isMainSpeaker')
         .from('ConferenceXSpeaker AS c')
         .innerJoin('Speaker AS s', 'c.SpeakerId', '=', 's.Id')
         .whereIn('c.ConferenceId', ids)
-        .then(rows => ids.map(id => rows.filter(row => row.id === id)))
+        .then(rows => ids.map(id => rows.filter(row => row.conferenceId === id)))
     )
-    
-    
   }
 }
 
